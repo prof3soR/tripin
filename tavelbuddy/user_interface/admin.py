05fbcddc0ff0735543b2e784313d662bc93312ec
Rohit kumar <76482418+prof3soR@users.noreply.github.com>
2023-04-09 20:29:41 +0530

    Delete tavelbuddy directory

--- tavelbuddy/user_interface/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(profile)
-admin.site.register(tripdetails)
-admin.site.register(secretspot)
-admin.site.register(review_loaction)
-admin.site.register(category)
-admin.site.register(destination)
